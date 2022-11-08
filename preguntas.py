260d74604d07b96601c0cb8084e2ff7b791660b3
mmunozal <55594437+mmunozal@users.noreply.github.com>
2022-11-07 20:37:58 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -97,7 +97,10 @@ def pregunta_04():
     # Importe GridSearchCV
     # Importe Pipeline
     # Importe BernoulliNB
-    from ____ import ____
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.pipeline import Pipeline
+    from sklearn.naive_bayes import BernoulliNB
 
     # Cargue las variables.
     x_train, _, y_train, _ = pregunta_02()
@@ -110,40 +113,40 @@ def pregunta_04():
     # límite superior para la frecuencia de palabras es del 100% y un límite
     # inferior de 5 palabras. Solo deben analizarse palabras conformadas por
     # letras.
-    countVectorizer = ____(
-        analyzer=____,
-        lowercase=____,
-        stop_words=____,
-        token_pattern=____,
-        binary=____,
-        max_df=____,
-        min_df=____,
+    countVectorizer = CountVectorizer(
+        analyzer= analyzer,
+        lowercase= True,
+        stop_words= "english" ,
+        token_pattern= r"\b\w\w+\b",
+        binary=True,
+        max_df= 1.0,
+        min_df= 5,
     )
 
     # Cree un pipeline que contenga el CountVectorizer y el modelo de BernoulliNB.
-    pipeline = ____(
+    pipeline = Pipeline(
         steps=[
-            ("____", ____),
-            ("____", ____()),
-        ],
+            ("CountVectorizer", countVectorizer),
+            ("BernoulliNB", BernoulliNB()),
+        ]
     )
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "____": np.____(____, ____, ____),
+        "BernoulliNB__alpha": np.linspace(0.1, 1, 10)
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y "accuracy" como métrica de evaluación
-    gridSearchCV = ____(
-        estimator=____,
-        param_grid=____,
-        cv=____,
-        scoring=____,
-        refit=____,
-        return_train_score=____,
+    gridSearchCV = GridSearchCV(
+        estimator= pipeline,
+        param_grid=param_grid,
+        cv=5,
+        scoring= "accuracy",
+        refit=True,
+        return_train_score= False,
     )
 
     # Búsque la mejor combinación de regresores
@@ -160,7 +163,7 @@ def pregunta_05():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix
 
     # Obtenga el pipeline de la pregunta 3.
     gridSearchCV = pregunta_04()
@@ -169,14 +172,14 @@ def pregunta_05():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true = y_train,
+        y_pred = gridSearchCV.predict(X_train)
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true = y_test,
+        y_pred = gridSearchCV.predict(X_test)
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
